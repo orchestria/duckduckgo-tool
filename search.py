4f6ae5f4446ad1d86c66e4b27c943beb0115479f
Silvano Cerza <silvanocerza@gmail.com>
2024-08-10 15:21:25 +0200

    Simplify tool

--- search.py
+++ search.py
@@ -1,3 +1,9 @@
+# /// script
+# requires-python = "~=3.12"
+# dependencies = ["duckduckgo-search"]
+# ///
+
+
 import json
 import sys
 
@@ -8,7 +14,8 @@ from duckduckgo_search.exceptions import (
     TimeoutException,
 )
 
-if __name__ == "__main__":
+
+def search():
     args = sys.argv[-1]
     data = json.loads(args)
     if "query" not in data:
@@ -25,3 +32,7 @@ if __name__ == "__main__":
         raise e
 
     print(json.dumps([a["text"] for a in answers]))
+
+
+if __name__ == "__main__":
+    search()
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present Silvano Cerza <silvanocerza@gmail.com>
-#
-# SPDX-License-Identifier: MIT
